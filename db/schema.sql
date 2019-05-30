5d6c2c699fc525af6776a72975fe148ab7e2e88b
rob <robmathew21@gmail.com>
2019-05-30 00:46:58 -0500

    created database

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS tacos_db;
+USE tacos_db;
+
+CREATE TABLE tacos(
+	id int NOT NULL AUTO_INCREMENT,
+    taco_name varchar(255) NOT NULL,
+    shell varchar(255) NOT NULL,
+    vegetarian BOOLEAN,
+    picked_up BOOLEAN,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO tacos (taco_name, shell, vegetarian) VALUES ('Baja Shrimp', 'soft, flour', FALSE);
+INSERT INTO tacos (taco_name, shell, vegetarian) VALUES ('Chicken Tikka', 'soft, flour', FALSE);
+INSERT INTO tacos (taco_name, shell, vegetarian) VALUES ('Tater Taco', 'soft, flour', FALSE);
